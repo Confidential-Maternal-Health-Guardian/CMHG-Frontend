4e3e95e82f85d583d511a546e0fc68857413dcdd
Can Usluel <canusluel@gmail.com>
2023-01-18 16:47:28 +0300

    implemented prediction component

--- src/Components/PredictionComponent.tsx
+++ src/Components/PredictionComponent.tsx
@@ -1,9 +1,72 @@
 
-import { Button, Form, Input } from 'antd';
+import { Button, Form, Input, Modal, Select } from 'antd';
+import { useNavigate } from 'react-router-dom';
+import { getCookie } from '../Util/Cookie';
+import { baseUrl, refreshTokens } from '../Util/Token';
 
 function PredictionComponent() {
-  const onFinish = (values: any) => {
-    console.log('Success:', values);
+  const navigate = useNavigate()
+
+  const success = (res: string) => {
+
+    const arr = res.split(" ");
+
+    for (var i = 0; i < arr.length; i++) {
+      arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
+
+    }
+    const predictionResult = arr.join(" ");
+
+    Modal.success({
+      content: 'Your Prediction Result is: ' + predictionResult,
+    });
+  };
+
+  const epsilonError = () => {
+    Modal.error({
+      title: 'Error',
+      content: 'You exeeded the epsilon capacity.',
+    });
+  };
+
+  const onFinish = async (values: any) => {
+    const token = getCookie("access-token")
+    if (baseUrl !== undefined) {
+      const response = await fetch(baseUrl + "/predict", {
+        method: 'POST',
+        body: JSON.stringify({
+          "age": values.age,
+          "systolicBp": values.sbp,
+          "diastolicBp": values.dbp,
+          "bs": values.bs,
+          "bodyTemp": values.bt,
+          "heartRate": values.hr,
+          "modelType": values.model,
+          "epsilon": values.epsilon
+        }),
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer ' + token
+        },
+      });
+      const data = await response.json()
+
+      if (response.status === 200) {
+        if (data["riskLevel"] !== null) {
+          success(data["riskLevel"])
+        } else {
+          epsilonError()
+        }
+      } else if (response.status === 403) {
+        refreshTokens().then((res) => {
+          if (res) {
+            onFinish(values)
+          } else {
+            navigate("/")
+          }
+        })
+      }
+    }
   };
 
   const onFinishFailed = (errorInfo: any) => {
@@ -67,7 +130,22 @@ function PredictionComponent() {
         >
           <Input placeholder="Heart Rate" />
         </Form.Item>
-
+        <Form.Item name="epsilon" label="Epsilon Value" style={{ textAlign: "left" }}>
+          <Select placeholder="Epsilon Value">
+            <Select.Option value="0.5">0.5</Select.Option>
+            <Select.Option value="1.0">1.0</Select.Option>
+            <Select.Option value="1.5">1.5</Select.Option>
+            <Select.Option value="2.0">2.0</Select.Option>
+          </Select>
+        </Form.Item>
+        <Form.Item name="model" label="Model" style={{ textAlign: "left" }}>
+          <Select placeholder="Model Type">
+            <Select.Option value="dpr">Differential Private Random Forest</Select.Option>
+            <Select.Option value="rf">Random Forest</Select.Option>
+            <Select.Option value="dpsgd">Differential Private Stochastic Gradient Descent</Select.Option>
+            <Select.Option value="sgd">Stochastic Gradient Descent</Select.Option>
+          </Select>
+        </Form.Item>
         <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
           <Button type="primary" htmlType="submit" className="prediction-form-button">
             Submit
